69cb2bc4b396dd5a00eeabc96890455d99d81d38
ali gamal <aligamy58@gmail.com>
2019-04-25 14:40:19 +0200

    singleton on Form Class

---
+++ README.md
@@ -0,0 +1,2 @@
+# java-editor
+This is an editor with simple tasks and functions under upgrading
---
+++ src/controller/MainController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import form.MainForm;
+
+/**
+ *
+ * @author Sharp team
+ */
+public class MainController {
+    public static void main(String[] args) {
+        //MainForm mainForm = new MainForm();
+        MainForm mainForm = MainForm.getInstance();
+        mainForm.setVisible(true);
+        EditController edit = new EditController();
+        edit.controller(mainForm);
+        FileController file = new FileController();
+        file.controller(mainForm);
+    }
+}
